7ce71488a9a5929779cddf2410ee6d46e40e9869
DaggerHashimmoto <DaggerHashimmoto@gmail.com>
2022-10-30 08:08:37 +0500

    //

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,7 @@
 // console.log('Yo')
 
-function stringHandler(word) {
-    console.log(word.replace(function(a) {return a.toUpperCase()}))
-}
-
-stringHandler('Harley davidson')
+// function stringHandler(word) {
+//     console.log(word.replace(function(a) {return a.toUpperCase()}))
+// }
+//
+// stringHandler('Harley davidson')
